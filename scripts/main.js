fb5a4b612f696615b6f455124e9c05b4b0db56de
edwinchen85 <edwinchen85@gmail.com>
2017-11-20 21:41:15 +0800

    Make slick slider responsive in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -57,7 +57,22 @@ $(document).ready(function() {
     slidesToShow: 3,
     fade: false,
     prevArrow: '<button type="button" class="slick-prev">Prev</button>',
-    nextArrow: '<button type="button" class="slick-next">Next</button>'
+    nextArrow: '<button type="button" class="slick-next">Next</button>',
+
+    responsive: [
+      {
+        breakpoint: 990,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 
 });
